3915e50ec9692204b6931addbb18af5371f6ec7f
Ayush Tripathi <ayushtripathi445@gmail.com>
2021-11-21 22:08:19 +0530

    first commit

---
+++ app/src/main/java/com/cometchat/pro/androiduikit/UIKitApplication.java
@@ -0,0 +1,72 @@
+package com.cometchat.pro.androiduikit;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.widget.Toast;
+
+
+import com.cometchat.pro.core.AppSettings;
+import com.cometchat.pro.core.CometChat;
+import com.cometchat.pro.exceptions.CometChatException;
+import com.cometchat.pro.androiduikit.constants.AppConfig;
+import com.cometchat.pro.uikit.ui_components.calls.call_manager.listener.CometChatCallListener;
+import com.cometchat.pro.uikit.ui_settings.UIKitSettings;
+
+
+public class UIKitApplication extends Application {
+
+    private static final String TAG = "UIKitApplication";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AppSettings appSettings = new AppSettings.AppSettingsBuilder().
+                subscribePresenceForAllUsers().setRegion(AppConfig.AppDetails.REGION).build();
+        CometChat.init(this, AppConfig.AppDetails.APP_ID, appSettings,
+                new CometChat.CallbackListener<String>() {
+            @Override
+            public void onSuccess(String s) {
+                UIKitSettings.setAppID(AppConfig.AppDetails.APP_ID);
+                UIKitSettings.setAuthKey(AppConfig.AppDetails.AUTH_KEY);
+                CometChat.setSource("ui-kit","android","java");
+            }
+
+            @Override
+            public void onError(CometChatException e) {
+                Toast.makeText(UIKitApplication.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        UIKitSettings uiKitSettings = new UIKitSettings(this);
+        uiKitSettings.addConnectionListener(TAG);
+        CometChatCallListener.addCallListener(TAG,this);
+        createNotificationChannel();
+    }
+
+
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.app_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel("2", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        CometChat.removeConnectionListener(TAG);
+        CometChatCallListener.removeCallListener(TAG);
+    }
+}
---
+++ app/src/main/java/com/cometchat/pro/androiduikit/constants/AppConfig.java
@@ -0,0 +1,14 @@
+package com.cometchat.pro.androiduikit.constants;
+
+public class AppConfig {
+
+    public class AppDetails {
+
+        public static final String APP_ID = "197895ed5063772c";
+
+        public static final String AUTH_KEY = "c970c7d75250b5b3a12c5a22398be012ecba62ba";
+
+        public static final String REGION = "us";
+    }
+}
+
